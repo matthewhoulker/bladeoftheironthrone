4d63727a62488ea8c37c35fb170ab2b40d241c84
Matthew <houlker@gmail.com>
2021-10-25 13:57:29 +0100

    working on derived atts

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -11,19 +11,28 @@ BLADEOFTHEIRONTHRONE.foobar = {
  * @type {Object}
  */
  BLADEOFTHEIRONTHRONE.abilities = {
-  "str": "BLADEOFTHEIRONTHRONE.AbilityStr",
-  "dex": "BLADEOFTHEIRONTHRONE.AbilityDex",
-  "con": "BLADEOFTHEIRONTHRONE.AbilityCon",
-  "int": "BLADEOFTHEIRONTHRONE.AbilityInt",
-  "wis": "BLADEOFTHEIRONTHRONE.AbilityWis",
-  "cha": "BLADEOFTHEIRONTHRONE.AbilityCha"
+  "brawn": "BLADEOFTHEIRONTHRONE.AbilityBrawn",
+  "daring": "BLADEOFTHEIRONTHRONE.AbilityDaring",
+  "tenacity": "BLADEOFTHEIRONTHRONE.AbilityTenacity",
+  "heart": "BLADEOFTHEIRONTHRONE.AbilityHeart",
+  "sagacity": "BLADEOFTHEIRONTHRONE.AbilitySagacity",
+  "cunning": "BLADEOFTHEIRONTHRONE.AbilityCunning"
 };
 
 BLADEOFTHEIRONTHRONE.abilityAbbreviations = {
-  "str": "BLADEOFTHEIRONTHRONE.AbilityStrAbbr",
-  "dex": "BLADEOFTHEIRONTHRONE.AbilityDexAbbr",
-  "con": "BLADEOFTHEIRONTHRONE.AbilityConAbbr",
-  "int": "BLADEOFTHEIRONTHRONE.AbilityIntAbbr",
-  "wis": "BLADEOFTHEIRONTHRONE.AbilityWisAbbr",
-  "cha": "BLADEOFTHEIRONTHRONE.AbilityChaAbbr"
+  "brawn": "BLADEOFTHEIRONTHRONE.AbilityBrawnAbbr",
+  "daring": "BLADEOFTHEIRONTHRONE.AbilityDaringAbbr",
+  "tenacity": "BLADEOFTHEIRONTHRONE.AbilityTenacityAbbr",
+  "heart": "BLADEOFTHEIRONTHRONE.AbilityHeartAbbr",
+  "sagacity": "BLADEOFTHEIRONTHRONE.AbilitySagacityAbbr",
+  "cunning": "BLADEOFTHEIRONTHRONE.AbilityCunningAbbr"
+};
+
+BLADEOFTHEIRONTHRONE.derivedAbilities = {
+  "reflex": "BLADEOFTHEIRONTHRONE.derivedAbilityReflex",
+  "aim": "BLADEOFTHEIRONTHRONE.derivedAbilityAim",
+  "knockdown": "BLADEOFTHEIRONTHRONE.derivedAbilityKnockdown",
+  "knockout": "BLADEOFTHEIRONTHRONE.derivedAbilityKnockout",
+  "move": "BLADEOFTHEIRONTHRONE.derivedAbilityMove",
+  "power": "BLADEOFTHEIRONTHRONE.derivedAbilityPower",
 };
